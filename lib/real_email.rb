720acb77e372106006ca3042c5780e365247e24a
mbuckbee <michaelbuckbee@gmail.com>
2016-06-03 06:56:46 +0000

    Init

---
+++ lib/real_email.rb
@@ -0,0 +1,17 @@
+# This file was automatically generated for Expedited Addons by APIMATIC v2.0 ( https://apimatic.io ) on 06/03/2016
+require 'openssl'
+require 'json'
+require 'unirest'
+
+# APIMATIC Helper Files
+require_relative 'real_email/api_helper.rb'
+require_relative 'real_email/api_exception.rb'
+require_relative 'real_email/configuration.rb'
+require_relative 'real_email/real_email_client.rb'
+
+# Controllers
+require_relative 'real_email/controllers/base_controller.rb'
+require_relative 'real_email/controllers/api_controller.rb'
+
+# Models
+require_relative 'real_email/models/result.rb'
